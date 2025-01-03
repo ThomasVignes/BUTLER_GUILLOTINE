ef6191094132693d7da3658408e638389b375d63
WhumpusWu <whumpus.wu@gmail.com>
2025-01-03 21:54:18 +0100

    e

--- TRUE_ENTROPY_UNITYPROJECT/Assets/ObjectSwapper.cs
+++ TRUE_ENTROPY_UNITYPROJECT/Assets/ObjectSwapper.cs
@@ -4,35 +4,43 @@ using UnityEngine;
 
 public class ObjectSwapper : MonoBehaviour
 {
-    [SerializeField] List<GameObject> OnEnterHide = new List<GameObject>();
-    [SerializeField] List<GameObject> OnExitHide = new List<GameObject>();
+    [Header("LinkedCameras")]
+    [SerializeField] CameraZone[] cameras;
+
+    [Header("Objects")]
+    [SerializeField] List<GameObject> Specific = new List<GameObject>();
+    [SerializeField] List<GameObject> Hide = new List<GameObject>();
 
     private void Awake()
     {
-        foreach (var item in OnEnterHide)
+        foreach (var cam in cameras)
         {
-            if (!item.activeSelf)
-                item.SetActive(true);
-        }
-
-        foreach (var item in OnExitHide)
-        {
-            if (item.activeSelf)
+            foreach (var item in Specific)
+            {
+                cam.ShotSpecificObjects.Add(item);
                 item.SetActive(false);
+            }
+
+            foreach (var item in Hide)
+            {
+                cam.ShotSpecificHide.Add(item);
+                item.SetActive(true);
+            }
         }
     }
 
+    /*
     private void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<CameraDetector>() != null)
         {
-            foreach (var item in OnEnterHide)
+            foreach (var item in Specific)
             {
                 if (item.activeSelf)
                     item.SetActive(false);
             }
 
-            foreach (var item in OnExitHide) 
+            foreach (var item in Hide) 
             {
                 if (!item.activeSelf)
                     item.SetActive(true);
@@ -45,17 +53,18 @@ public class ObjectSwapper : MonoBehaviour
     {
         if (other.GetComponent<CameraDetector>() != null)
         {
-            foreach (var item in OnEnterHide)
+            foreach (var item in Specific)
             {
                 if (!item.activeSelf)
                     item.SetActive(true);
             }
 
-            foreach (var item in OnExitHide)
+            foreach (var item in Hide)
             {
                 if (item.activeSelf)
                     item.SetActive(false);
             }
         }
     }
+    */
 }
