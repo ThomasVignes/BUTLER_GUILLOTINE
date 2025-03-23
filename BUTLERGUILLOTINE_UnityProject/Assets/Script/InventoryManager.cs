8b1a804fc313a59ac82a3dcc7d0e4f6a40d5eda0
WhumpusWu <whumpus.wu@gmail.com>
2025-03-23 17:38:19 +0100

    Keyring vanishes after multiple uses

--- BUTLERGUILLOTINE_UnityProject/Assets/Script/InventoryManager.cs
+++ BUTLERGUILLOTINE_UnityProject/Assets/Script/InventoryManager.cs
@@ -14,17 +14,38 @@ public class InventoryManager : MonoBehaviour
 
     public void Init(Item[] items)
     {
-        this.items = items;
+        this.items = new Item[items.Length];
 
-        foreach (var item in items)
+        for (int i = 0; i < this.items.Length; i++)
         {
-            item.Equipped = false;
+            var it = items[i];
+
+            this.items[i] = new Item(it.ID, it.Name, it.Sprite, it.LimitedUses, it.Uses);
         }
     }
 
     public void HideCanvas(bool hidden)
     {
         canvas.SetActive(!hidden);
+    }
+
+    public void UseItem(string name)
+    {
+        Item item = Array.Find(items, e => e.ID == name);
+
+        if (item == null || !item.Equipped)
+            return;
+
+        if (!item.LimitedUses)
+            return;
+
+        item.Uses--;
+
+        if (item.Uses <= 0)
+        {
+            Destroy(item.Icon);
+            item.Equipped = false;
+        }
     }
 
     public void EquipItem(string name)
@@ -59,6 +80,17 @@ public class Item
     public string ID;
     public string Name;
     public Sprite Sprite;
+    public bool LimitedUses;
+    public int Uses;
     [HideInInspector] public bool Equipped;
     [HideInInspector] public GameObject Icon;
+
+    public Item(string id, string name, Sprite sprite, bool limitedUses, int uses)
+    {
+        ID = id;
+        Name = name;
+        Sprite = sprite;
+        LimitedUses = limitedUses;
+        Uses = uses;
+    }
 }
