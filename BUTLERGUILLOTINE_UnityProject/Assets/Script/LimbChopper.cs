947b28d44917ff1649ad9af29cf71a919345477a
WhumpusWu <whumpus.wu@gmail.com>
2025-04-21 00:07:05 +0200

    Cool

--- BUTLERGUILLOTINE_UnityProject/Assets/Script/LimbChopper.cs
+++ BUTLERGUILLOTINE_UnityProject/Assets/Script/LimbChopper.cs
@@ -8,11 +8,18 @@ public class LimbChopper : MonoBehaviour
     [SerializeField] GameObject previous, replacement;
     [SerializeField] Animator animator;
     [SerializeField] string chopTrigger;
+    [SerializeField] GameObject additionalPrevious, additionalReplacement; 
 
     public void Chop()
     {
         previous.transform.localScale = Vector3.zero;
         replacement.SetActive(true);
+
+        if (additionalPrevious != null) 
+            additionalPrevious.transform.localScale = Vector3.zero;
+
+        if (additionalReplacement != null)
+            additionalReplacement.SetActive(true);
 
         if (animator != null )
             animator.SetTrigger(chopTrigger);
