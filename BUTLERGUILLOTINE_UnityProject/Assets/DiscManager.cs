ce4aecd93659db4d65cf1793ab6c68b68d428206
WhumpusWu <whumpus.wu@gmail.com>
2025-11-17 23:29:27 +0100

    credits WIP

--- BUTLERGUILLOTINE_UnityProject/Assets/DiscManager.cs
+++ BUTLERGUILLOTINE_UnityProject/Assets/DiscManager.cs
@@ -5,17 +5,72 @@ using UnityEngine;
 
 public class DiscManager : MonoBehaviour
 {
+    [Header("Data")]
     [SerializeField] List<DiscSong> songs = new List<DiscSong>();
+
+    [Header("References")]
     [SerializeField] Animator ruthAnimator;
+    [SerializeField] Animator discPlayerAnimator, cameraAnimator;
     [SerializeField] TextMeshProUGUI title;
 
     public int currentIndex;
-    bool hasDisc;
+    bool hasDisc, playing;
 
     private void Start()
     {
         title.text = "";
         currentIndex = -1;
+    }
+
+    public void Play()
+    {
+        if (currentIndex == -1) return;
+
+        if (playing) return;
+
+        playing = true;
+
+        StartCoroutine(C_PlaySong());
+    }
+
+    IEnumerator C_PlaySong()
+    {
+        ruthAnimator.SetTrigger("Play");
+
+        yield return new WaitForSeconds(0.4f);
+
+        cameraAnimator.SetBool("Playing", true);
+
+        yield return new WaitForSeconds(0.05f);
+
+        discPlayerAnimator.SetTrigger("Start");
+
+        yield return new WaitForSeconds(2f);
+
+        discPlayerAnimator.SetTrigger("Spin");
+    }
+
+    public void Stop()
+    {
+        if (!playing) return;
+
+        playing = false;
+
+        StartCoroutine(C_StopSong());
+    }
+
+    IEnumerator C_StopSong()
+    {
+        discPlayerAnimator.SetTrigger("Stop");
+
+        yield return new WaitForSeconds(0.5f);
+
+        cameraAnimator.SetBool("Playing", false);
+
+        yield return new WaitForSeconds(0.45f);
+
+
+        ruthAnimator.SetTrigger("Stop");
     }
 
     public void NextDisc()
